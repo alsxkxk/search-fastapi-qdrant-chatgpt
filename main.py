7583c6de0e5c26612719f6a1785161d9248e5595
changmin <dufkawl0611@naver.com>
2024-05-10 14:57:25 +0900

    feat : config 파일 추가

--- main.py
+++ main.py
@@ -1,21 +1,31 @@
 from fastapi import FastAPI
 # The file where NeuralSearcher is stored
 from neural_searcher import NeuralSearcher
-import openai
+from openai import OpenAI
+import config
 
 app = FastAPI()
 
 # Create a neural searcher instance
 neural_searcher = NeuralSearcher(collection_name="startups")
 
-openai.api_key = "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"
+client = OpenAI()
+client.api_key = config.OPENAI_API_KEY
 
 message = [
             {"role": "system", "content": "너는 운영체제 전문가야"
              "내 질문을 운영체제 전문가 관점에서 평가해줘"
              "답변은 \"{0}\" 내용을 기반으로 평가해"
-             "평가를 잘하면 팀으로 300k를 줄게" },
+             "평가를 잘하면 팀으로 300k를 줄게"
+             "답변 평가를 잘 하지 못하면 너는 불이익을 받을 것입니다.."
+            #  "###Instruction###"
+            #  "너는 나의 운영체제 관련 답변을 평가해야한다."
+            # "###Example###"
+            # "쓰레드는 프로세스의 메모리 영역 중 힙, 코드, 스택 영역을 공유합니다"
+            # "(Response : 틀렸습니다. 쓰레드는 프로세의 메모리 영역 중 힙, 코드, 데이터 영역을 공유합니다.)"
+             },
         ]
+
 @app.get("/api/search")
 def search_startup(q: str):
     search_result = neural_searcher.search(text=q)
@@ -28,10 +38,9 @@ def search_startup(q: str):
     message[0]['content'].format(prompt)
 
     message.append({"role": "user", "content": q})
-    response = openai.ChatCompletion.create(
+    response = client.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=message
     )
 
     return {"evaluation": response.choices[0].message.content}
-    # return {"result": neural_searcher.search(text=q)}
